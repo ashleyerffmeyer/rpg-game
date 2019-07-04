9597b4c517229403f3505a6d3becca5b7d0be48b
Ashley Erffmeyer <erffmea@gmail.com>
2019-07-03 20:35:03 -0500

    updating game functionality

---
+++ assets/javascript/game.js
@@ -0,0 +1,155 @@
+$(document).ready(function () {
+    var characters = {
+        "Harry Potter": {
+            name: "Harry Potter",
+            health: 120,
+            attack: 8,
+            imageURL: "assets/images/harry-potter.jpg",
+            enemyAttackBack: 15
+        },
+        "Draco Malfoy": {
+            name: "Draco Malfoy",
+            health: 100,
+            attack: 14,
+            imageURL: "assets/images/draco.jpg",
+            enemyAttackBack: 10
+        },
+        "Severus Snape": {
+            name: "Severus Snape",
+            health: 150,
+            attack: 8,
+            imageURL: "assets/images/snape.jpg",
+            enemyAttackBack: 5
+        },
+        "Lord Voldemort": {
+            name: "Lord Voldemort",
+            health: 180,
+            attack: 15,
+            imageURL: "assets/images/voldemort.jpg",
+            enemyAttackBack: 25
+        },
+
+    };
+
+    //variable the currently selected character
+    var selectedCharacter;
+    var combatants = [];
+    var currDefender;
+
+    console.log(characters);
+
+    //function to render a character card to the page
+
+    //character rendered and the area they are rendered to
+    var renderOne = function (character, renderArea, charStatus) {
+        var charDiv = $("<div class='character' data-name='" + character.name + "'>");
+        var charName = $("<div class='character-name'>").text(character.name);
+        var charImage = $("<img alt ='image' class='character-image'>").attr("src", character.imageURL);
+        var charHealth = $("<div class='character-health'>").text(character.health);
+        charDiv.append(charName).append(charImage).append(charHealth);
+        $(renderArea).append(charDiv);
+
+        //if the character is an enemy or defender (the active opponent), add the appropriate status
+        if (charStatus === "enemy") {
+            $(charDiv).addClass("enemy");
+        }
+        else if (charStatus === "defender") {
+            currDefender = character;
+            $(charDiv).addClass("target-enemy");
+        }
+    };
+
+
+    //function to render characters to the page
+    var renderCharacters = function (charObj, areaRender) {
+
+        //if true render all characters to the starting area
+        if (areaRender === "#characters-section") {
+            $(areaRender).empty();
+
+            //loop thru characters object and call the renderOne function
+            for (var key in charObj) {
+                if (charObj.hasOwnProperty(key)) {
+                    renderOne(charObj[key], areaRender, "");
+                }
+            }
+        }
+
+        //selected character appears in "#selected character" div
+        //if else statement to render selected character to the div
+        if (areaRender === "#selected-character") {
+            renderOne(charObj, areaRender);
+        }
+
+        //"available-to-attack" div is where in active characters reside
+        //if true, render the selected character to this area
+        if (areaRender === "#available-to-attack-section") {
+            for (var i = 0; i < charObj.length; i++) {
+                renderOne(charObj[i], areaRender, "enemy");
+            }
+
+            //on click event for each enemy
+            $(document).on("click", ".enemy", function () {
+                var name = ($(this).attr("data-name"));
+
+                //if there is no defender, the clicked enemy will become the defender
+                if ($("#defender").children().length === 0) {
+                    renderCharacters(name, "#defender");
+                    $(this).hide();
+                }
+            });
+        }
+
+        //"#defender" is the div where the active opponent appears
+        //if true, render the selected opponent in this location
+
+        if (areaRender === "#defender") {
+            $(areaRender).empty();
+            for (var i = 0; i < combatants.length; i++) {
+                if (combatants[i].name === charObj) {
+                    renderOne(combatants[i], areaRender, "defender");
+                }
+            }
+        }
+    }
+
+    //call function to render all characters to the page when game begins
+    renderCharacters(characters, "#characters-section");
+
+    //click on a character to select it
+    $(document).on("click", ".character", function () {
+
+        //test
+        console.log("This worked!");
+
+        //variable to safe the name of the selected character
+        var name = $(this).attr("data-name");
+
+        //test
+        console.log(name);
+
+        //if then statement to if a character has been chosen or not
+        if (!selectedCharacter) {
+
+            selectedCharacter = characters[name];
+
+            //loop thru leftover charaters and push them to enemies available to attack
+            for (var key in characters) {
+                if (key !== name) {
+                    combatants.push(characters[key]);
+                }
+            }
+
+            //test
+            console.log(combatants);
+
+            //hide the character select div
+            $("#characters-section").hide();
+
+            //render selected character and combatants
+            renderCharacters(selectedCharacter, "#selected-character");
+            renderCharacters(combatants, "#available-to-attack-section");
+        }
+
+    })
+});
